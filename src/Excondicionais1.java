f0eab935ca50e28ea2d0b559e74702b9ff4351c1
Jeane <jeane.aula@gmail.com>
2022-11-16 16:32:41 -0300

    Segunda lista de exercícios

---
+++ src/Excondicionais1.java
@@ -0,0 +1,22 @@
+public class Excondicionais1 {
+    public static void main(String[] args) {
+
+        //"1.Determinar se um aluno passou de ano");
+        int nota1 = 5 ;
+        int nota2 = 8;
+        int nota3 = 6;
+        int nota4 = 7;
+
+
+       double media = (nota1 + nota2 + nota3 + nota4)/4;
+
+
+
+        if (media >= 6){
+            System.out.println("aprovado");
+        } else {
+            System.out.println("Reprovado");
+        }
+
+    }
+}
